f41fe4651f87ee1267f9234865a9d75b2b7feaba
Julian Sandersius <sandersiusjulian@gmail.com>
2018-07-06 16:52:38 -0700

    serverrunning with connection to mLab db

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-// server.js
+
 const express = require('express');
 const MongoClient = require('mongodb').MongoClient;
 const bodyParser = require('body-parser');
@@ -8,10 +8,7 @@ const port = 8000;
 app.use(bodyParser.urlencoded({ extended: true }));
 MongoClient.connect(db.url, (err, database) => {
     if (err) return console.log(err)
-
-    // Make sure you add the database name and not the collection name
-    db = database.db("juliannodeapi")
-    require('./app/routes')(app, db);
+    require('./app/routes')(app, database);
     app.listen(port, () => {
         console.log('We are live on ' + port);
     });
