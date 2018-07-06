8c08c1031841cb4904eed4feb894e1b03c487a7a
Julian Sandersius <sandersiusjulian@gmail.com>
2018-07-06 15:32:42 -0700

    getting authentication failed

--- server.js
+++ server.js
@@ -12,9 +12,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 MongoClient.connect(db.url, (err, database) => {
     if (err) return console.log(err)
 
-    // Make sure and add the databse name and not the collection name
-
-    //db = database.db("juliannodeapi")
+    db = database.db("juliannodeapi")
     require('./app/routes')(app, db);
 
     app.listen(port, () => {
