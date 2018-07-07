4d6e2fe7c79c3b3399cd12e11806af148763e95d
Julian Sandersius <sandersiusjulian@gmail.com>
2018-07-06 18:19:42 -0700

    untracked changes

--- app/routes/note_routes.js
+++ app/routes/note_routes.js
@@ -1,17 +1,28 @@
+var ObjectID = require('mongodb').ObjectID;
+
+
 module.exports = function (app, db) {
     app.get('./notes/:id', (req, res) => {
-
+        const details = {
+            '_id': <ID GOES HERE> };
+        db.collection("notes").findOne(details, (err, item) => {
+            if (err) {
+                    res.send({ "error": "An error has occurred" });
+                } else {
+                    res.send(item);
+                }
+        });
     });
 
 
     app.post('/notes', (req, res) => {
-        const note = { text: req.body.body, title: req.body.title };
+        const note = {text: req.body.body, title: req.body.title };
         db.collection('notes').insert(note, (err, result) => {
             if (err) {
-                res.send({ "error": "an error has occured" });
-            } else {
-                res.send(result.ops[0]);
-            }
+                    res.send({ "error": "an error has occured" });
+                } else {
+                    res.send(result.ops[0]);
+                }
         });
     });
 };
