1f8faf3a66e9624604d699771b431d37717c4185
Julian Sandersius <sandersiusjulian@gmail.com>
2018-07-07 15:11:42 -0700

    rearranged routes, CRUD done! API complete and moving on

--- app/routes/note_routes.js
+++ app/routes/note_routes.js
@@ -1,7 +1,19 @@
 var ObjectID = require('mongodb').ObjectID;
 module.exports = function (app, db) {
 
-    //CREATE ROUTE
+    //CREATE ROUTE 
+    app.post('/notes', (req, res) => {
+        const note = { text: req.body.body, title: req.body.title };
+        db.collection('notes').insert(note, (err, result) => {
+            if (err) {
+                res.send({ 'error': 'An error has occurred' });
+            } else {
+                res.send(result.ops[0]);
+            }
+        });
+    });
+
+    //READ ROUTE
     app.get('/notes/:id', (req, res) => {
         const id = req.params.id;
         const details = { '_id': new ObjectID(id) };
@@ -13,30 +25,8 @@ module.exports = function (app, db) {
             }
         });
     });
-    //READ ROUTE   
-    app.post('/notes', (req, res) => {
-        const note = { text: req.body.body, title: req.body.title };
-        db.collection('notes').insert(note, (err, result) => {
-            if (err) {
-                res.send({ 'error': 'An error has occurred' });
-            } else {
-                res.send(result.ops[0]);
-            }
-        });
-    });
+
     //UPDATE ROUTE
-    app.delete('/notes/:id', (req, res) => {
-        const id = req.params.id;
-        const details = { '_id': new ObjectID(id) };
-        db.collection('notes').remove(details, (err, item) => {
-            if (err) {
-                res.send({ 'error': 'An error has occured' });
-            } else {
-                res.send('Note' + id + ' deleted!');
-            }
-        });
-    });
-    //DELETE ROUTE
     app.put('/notes/:id', (req, res) => {
         const id = req.params.id;
         const details = { '_id': new ObjectID(id) };
@@ -46,6 +36,19 @@ module.exports = function (app, db) {
                 res.send({ 'error': 'An erro has occured' });
             } else {
                 res.send(note);
+            }
+        });
+    });
+
+    //DELETE ROUTE
+    app.delete('/notes/:id', (req, res) => {
+        const id = req.params.id;
+        const details = { '_id': new ObjectID(id) };
+        db.collection('notes').remove(details, (err, item) => {
+            if (err) {
+                res.send({ 'error': 'An error has occured' });
+            } else {
+                res.send('Note' + id + ' deleted!');
             }
         });
     });
