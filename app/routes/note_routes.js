b444fe3fc089c6b49b5fb91e356222758cf6aa89
Julian Sandersius <sandersiusjulian@gmail.com>
2018-07-06 18:19:18 -0700

    Post and Read routes, broken right now

--- app/routes/note_routes.js
+++ app/routes/note_routes.js
@@ -1,13 +1,17 @@
 module.exports = function (app, db) {
-    const collection =
-        app.post('/notes', (req, res) => {
-            const note = { text: req.body.body, title: req.body.title };
-            db.collection('notes').insert(note, (err, result) => {
-                if (err) {
-                    res.send({ "error": "an error has occured" });
-                } else {
-                    res.send(result.ops[0]);
-                }
-            });
+    app.get('./notes/:id', (req, res) => {
+
+    });
+
+
+    app.post('/notes', (req, res) => {
+        const note = { text: req.body.body, title: req.body.title };
+        db.collection('notes').insert(note, (err, result) => {
+            if (err) {
+                res.send({ "error": "an error has occured" });
+            } else {
+                res.send(result.ops[0]);
+            }
         });
+    });
 };
