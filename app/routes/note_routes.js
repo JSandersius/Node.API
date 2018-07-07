e95acb0cc498a5b11e49e2f6d4407764a6d24dfb
Julian Sandersius <sandersiusjulian@gmail.com>
2018-07-07 14:08:25 -0700

    rebuild routes and server, test complete

--- app/routes/note_routes.js
+++ app/routes/note_routes.js
@@ -1,7 +1,13 @@
-// note_routes.js
 module.exports = function (app, db) {
-    app.post('/notes', (req, res) => {
-        console.log(req.body)
-        res.send('Hello')
-    });
+    const collection =
+        app.post('/notes', (req, res) => {
+            const note = { text: req.body.body, title: req.body.title };
+            db.collection('notes').insert(note, (err, result) => {
+                if (err) {
+                    res.send({ 'error': 'An error has occurred' });
+                } else {
+                    res.send(result.ops[0]);
+                }
+            });
+        });
 };
